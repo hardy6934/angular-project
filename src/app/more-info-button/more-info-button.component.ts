1a49f82a00e6c6c58b8a4082ad59861ca1bf5b3b
Smamedov <smamedov451@gmail.com>
2025-02-17 14:44:14 +0300

    Created user profile window with 2 components 1 for user info and anothe 1 for more info button

---
+++ src/app/more-info-button/more-info-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more-info-button',
+  imports: [],
+  templateUrl: './more-info-button.component.html',
+  styleUrl: './more-info-button.component.scss'
+})
+export class MoreInfoButtonComponent {
+
+}
